ffe458c96c7f3c16a7fb8f1c428c35b2814bf868
Taha <x3727349s@iespoblenou.org>
2016-10-06 17:11:29 +0200

    Fitxer final

--- src/EjecutarFitxerPython.java
+++ src/EjecutarFitxerPython.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by x3727349s on 06/10/16.
  */
-public class Main {
+public class EjecutarFitxerPython {
 
     public static void main(String[] args) {
 
@@ -19,7 +16,7 @@ public class Main {
             Process proceso = Runtime.getRuntime().exec(comando);
 
             InputStreamReader entrada = new InputStreamReader(proceso.getInputStream());
-            BufferedReader stdInput =   new BufferedReader(entrada);
+            BufferedReader stdInput = new BufferedReader(entrada);
 
 
             while ((salida = stdInput.readLine()) !=null)
@@ -33,4 +30,4 @@ public class Main {
         }
     }
 
-    }
+}
